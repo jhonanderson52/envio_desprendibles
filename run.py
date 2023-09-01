1f554352b1201bac4520e4eec4c7d8a2406d17fc
jhonanderson52 <jhonanderson52@gmail.com>
2023-09-01 00:10:02 -0500

    Cambio Index.html

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-#from app.routes import app  # Importa la instancia de la aplicación Flask desde app.py
-
-#if __name__ == '__main__':
-#   app.run(debug=True)
 from app.routes import app  # Importa la instancia de la aplicación Flask desde app.py
-from waitress import serve  # Importa la función serve de Waitress
 
 if __name__ == '__main__':
+   app.run(debug=True)
+#from app.routes import app  # Importa la instancia de la aplicación Flask desde app.py
+#from waitress import serve  # Importa la función serve de Waitress
+
+#if __name__ == '__main__':
     # Ejecuta la aplicación con Waitress en el puerto 8080
-    serve(app, host='0.0.0.0', port=8080)
+#    serve(app, host='0.0.0.0', port=8080)
